b3f7a988b27a8a1628a3acdef3d3191180c1b249
grant8827 <you@example.com>
2024-11-16 16:48:09 -0500

    radio player edit

--- src/components/comps/homeComponents/RadioPlayer.jsx
+++ src/components/comps/homeComponents/RadioPlayer.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 function RadioPlayer() {
   return (
     <div>
-
- <audio controls="true" preload="none"><source src="https://stream-171.zeno.fm/spwupb70cf9uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJzcHd1cGI3MGNmOXV2IiwiaG9zdCI6InN0cmVhbS0xNzEuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IkdOa1hiUlo4UlVLZldqQmlVaGJoSlEiLCJpYXQiOjE3Mjg3NDA1NzUsImV4cCI6MTcyODc0MDYzNX0.FnoA3fcCFHqkdgCed5ru0mvRxh4VrlmG_FZIHodw-d0/stream?type=.mp3" type="audio/mp3">Your browser does not support the audio element.</source></audio>
+<audio controls>
+  <source src="https://stream-171.zeno.fm/spwupb70cf9uv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJzcHd1cGI3MGNmOXV2IiwiaG9zdCI6InN0cmVhbS0xNzEuemVuby5mbSIsInJ0dGwiOjUsImp0aSI6IkdOa1hiUlo4UlVLZldqQmlVaGJoSlEiLCJpYXQiOjE3Mjg3NDA1NzUsImV4cCI6MTcyODc0MDYzNX0.FnoA3fcCFHqkdgCed5ru0mvRxh4VrlmG_FZIHodw-d0
+" type="audio/ogg"></source>
+</audio>
    </div>
   )
 }
